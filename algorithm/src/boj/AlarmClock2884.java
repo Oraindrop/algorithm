a1b8d306a271caed3df64d9b2604d89dcdcc6347
DESKTOP-638OTU9\ChoiSing <chltmdals115@gmail.com>
2019-05-21 23:24:54 +0900

    boj 2884 : 알람시계

---
+++ algorithm/src/boj/AlarmClock2884.java
@@ -0,0 +1,32 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class AlarmClock2884 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int h = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		if (m - 45 < 0) {
+			h -= 1;
+			m = m + 60 - 45;
+		} else {
+			m -= 45;
+		}
+		
+		if (h < 0) {
+			h += 24;
+		}
+		
+		System.out.println(h + " " + m);
+	}
+
+}
