87d4f947c150db06a1f0e0521a4742aeb92da762
DESKTOP-638OTU9\ChoiSing <chltmdals115@gmail.com>
2019-06-18 23:50:45 +0900

    boj 1946 : 신입사원
    12시전 풀이 실패

---
+++ algorithm/src/boj/Newcomer1946.java
@@ -0,0 +1,64 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Newcomer1946 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < t; i++) {
+			int n = Integer.parseInt(br.readLine());
+			List<Newcomer> list = new ArrayList<>();
+			for (int j = 0; j < n; j++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				int aScore = Integer.parseInt(st.nextToken());
+				int bScore = Integer.parseInt(st.nextToken());
+				list.add(new Newcomer(aScore, bScore)); 
+			}
+			
+			Collections.sort(list, new Comparator<Newcomer>() {
+				@Override
+				public int compare(Newcomer o1, Newcomer o2) {
+					// TODO Auto-generated method stub
+					return o1.getaScore() - o2.getaScore();
+				}
+				
+			});
+			System.out.println(list);
+		}
+	}
+
+}
+
+class Newcomer {
+	private int aScore;
+	private int bScore;
+	
+	public Newcomer(int aScore, int bScore) {
+		this.aScore = aScore;
+		this.bScore = bScore;
+	}
+
+	public int getaScore() {
+		return aScore;
+	}
+
+	public int getbScore() {
+		return bScore;
+	}
+
+	@Override
+	public String toString() {
+		return "Newcomer [aScore=" + aScore + ", bScore=" + bScore + "]";
+	}
+}
