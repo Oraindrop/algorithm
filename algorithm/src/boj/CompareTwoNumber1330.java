bbf05f26aca845060ce63613c598f05e2fe49265
DESKTOP-638OTU9\ChoiSing <chltmdals115@gmail.com>
2019-06-29 13:20:03 +0900

    boj 1330 : 두 수 비교하기

---
+++ algorithm/src/boj/CompareTwoNumber1330.java
@@ -0,0 +1,27 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class CompareTwoNumber1330 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		if (a > b) {
+			System.out.println(">");
+		} else if (a < b) {
+			System.out.println("<");
+		} else {
+			System.out.println("==");
+		}
+	}
+
+}
