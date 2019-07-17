920492aca0c68ca756ebe2740d94e938f1d130e0
DESKTOP-638OTU9\ChoiSing <chltmdals115@gmail.com>
2019-07-17 23:48:07 +0900

    boj 10822 : 더하기

---
+++ algorithm/src/boj/Addition10822.java
@@ -0,0 +1,22 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Addition10822 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), ",");
+		int answer = 0;
+		while (st.hasMoreTokens()) {
+			answer += Integer.parseInt(st.nextToken());
+		}
+		
+		System.out.println(answer);
+	}
+
+}
