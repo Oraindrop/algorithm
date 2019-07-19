308adc4c12b164037254bded30c7a1b74fdb3841
DESKTOP-638OTU9\ChoiSing <chltmdals115@gmail.com>
2019-07-19 23:30:18 +0900

    boj 13322 : 접두사 배열

---
+++ algorithm/src/boj/PrefixArray13322.java
@@ -0,0 +1,22 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class PrefixArray13322 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		int size = s.length();
+		
+		for (int i = 0; i < size; i++) {
+			System.out.println(i);
+		}
+		
+	}
+
+}
+
