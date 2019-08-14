b42297b60f8b991409f4d5b045cfacb16f1d58de
DESKTOP-638OTU9\ChoiSing <chltmdals115@gmail.com>
2019-08-14 23:45:36 +0900

    boj 10821 : 정수의 개수

---
+++ algorithm/src/boj/IntegerCount10821.java
@@ -0,0 +1,17 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class IntegerCount10821 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String[] str = br.readLine().split(",");
+		System.out.println(str.length);
+		
+	}
+
+}
