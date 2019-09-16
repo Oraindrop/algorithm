27c25efb30a11dfde4d5c764565eb7cd3346b7b6
DESKTOP-638OTU9\ChoiSing <chltmdals115@gmail.com>
2019-09-16 21:39:24 +0900

    boj 10179 : 쿠폰

---
+++ algorithm/src/boj/Coupon10179.java
@@ -0,0 +1,21 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Coupon10179 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < t; i++) {
+			double d = Double.parseDouble(br.readLine());
+			sb.append(String.format("$%.2f", (d * 0.8))).append(System.lineSeparator());
+		}
+		System.out.print(sb.toString());
+	}
+
+}
