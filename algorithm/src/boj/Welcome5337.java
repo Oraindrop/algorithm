30446f18f0f849cf89dcd291cfe0549e91eaee9c
DESKTOP-638OTU9\ChoiSing <chltmdals115@gmail.com>
2019-11-28 23:55:28 +0900

    boj 잡 것 세개

---
+++ algorithm/src/boj/Welcome5337.java
@@ -0,0 +1,12 @@
+package boj;
+
+public class Welcome5337 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		StringBuilder sb = new StringBuilder();
+		sb.append(".  .   .").append(System.lineSeparator()).append("|  | _ | _. _ ._ _  _").append(System.lineSeparator()).append("|/\\|(/.|(_.(_)[ | )(/.");
+		System.out.println(sb.toString());
+	}
+
+}
---
+++ algorithm/src/boj/Yonsei15680.java
@@ -0,0 +1,17 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Yonsei15680 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		
+		System.out.println(t == 0 ? "YONSEI" : "Leading the Way to the Future");
+	}
+
+}
