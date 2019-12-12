d47cb49f0ecd536e471754b8aed534974ec4248b
DESKTOP-638OTU9\ChoiSing <chltmdals115@gmail.com>
2019-12-12 20:57:49 +0900

    boj 17390 : 이건 꼭 풀어야 해

---
+++ algorithm/src/boj/MustBeSolved17390.java
@@ -0,0 +1,44 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class MustBeSolved17390 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int q = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		
+		int[] arr = new int[n+1];
+		
+		for (int i = 1; i <= n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		long[] sum = new long[n+1];
+		sum[1] = arr[1];
+		
+		for (int i = 2; i <= n; i++) {
+			sum[i] = sum[i-1] + arr[i];
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < q; i++) {
+			st = new StringTokenizer(br.readLine());
+			int l = Integer.parseInt(st.nextToken());
+			int r = Integer.parseInt(st.nextToken());
+			sb.append(sum[r] - sum[l-1]).append(System.lineSeparator());
+		}
+		
+		System.out.print(sb.toString());
+	}
+
+}
