53e79fc0fc278768154f5508966f971697240fb6
DESKTOP-638OTU9\ChoiSing <chltmdals115@gmail.com>
2019-12-16 23:52:20 +0900

    boj 17389 : 보너스 점수

---
+++ algorithm/src/boj/BonusScore17389.java
@@ -0,0 +1,32 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BonusScore17389 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		int bonus = 0;
+		int score = 0;
+		String[] s = br.readLine().split("");
+		for (int i = 1; i <= n; i++) {
+			if (s[i-1].equals("O")) {
+				score += i;
+				score += bonus;
+				bonus++;
+			} else {
+				bonus = 0;
+			}
+		}
+		System.out.println(score);
+		
+		
+	}
+
+}
