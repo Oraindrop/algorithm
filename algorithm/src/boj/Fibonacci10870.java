eb434be8d239ff2bf8f71574f211c1e565d64acc
DESKTOP-638OTU9\ChoiSing <chltmdals115@gmail.com>
2020-01-25 23:31:41 +0900

    boj 10870 : 오타수정

--- algorithm/src/boj/Fibonacci10870.java
+++ algorithm/src/boj/Fibonacci10870.java
@@ -3,9 +3,8 @@ package boj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
-public class Fibonacci10870. {
+public class Fibonacci10870 {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
