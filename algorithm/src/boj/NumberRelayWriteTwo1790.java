297667d2f35069616ee68e666deca1201dcf14c7
DESKTOP-638OTU9\ChoiSing <chltmdals115@gmail.com>
2020-02-14 00:03:16 +0900

    boj 1790 : 수 이어쓰기2 마무리

--- algorithm/src/boj/NumberRelayWriteTwo1790.java
+++ algorithm/src/boj/NumberRelayWriteTwo1790.java
@@ -14,30 +14,27 @@ public class NumberRelayWriteTwo1790 {
 		long oneLocation = 1;
 		for (long i = 1; i <= 8; i++) {
 			oneLocation += i * (long)(9 * Math.pow(10, i-1));
-			arr[(int)i-1] = oneLocation;
+			arr[(int)i] = oneLocation;
 		}
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		String input = st.nextToken();
-		
 		long n = Long.parseLong(input);
 		long k = Long.parseLong(st.nextToken());
-		
 		System.out.println(go(arr, k, n));
 	}
 
 	private static int go(long[] arr, long k, long n) {
 		for (int i = 1; i < 9; i++) {
 			if (k < arr[i]) {
-				// i+1 자리수
 				long temp = k - arr[i-1];
-				long value = (temp / (i+1)) + (long)Math.pow(10, i) ;
+				long value = (temp / i) + (long)Math.pow(10, i-1) ;
 				if (value > n) {
 					return -1;
 				} else {
 					String s = String.valueOf(value);
-					long mod = temp % (i+1);
+					long mod = temp % i;
 					return Integer.parseInt(s.substring((int)mod, (int)(mod+1)));
 				}
 			} else if (k == arr[i]) {
@@ -45,10 +42,10 @@ public class NumberRelayWriteTwo1790 {
 			}
 		}
 		
-		if (k > arr[8] + 9) {
+		if (k > arr[8] + 8) {
 			return -1;
 		} else {
-			return k % arr[8] == 1 ? 1 : 0;	
+			return k % arr[8] == 0 ? 1 : 0;	
 		}
 	}
 
