bc91e0137e5be9e9e54c95af9a80ef94133a60da
Choising <chltmdals115@gmail.com>
2020-03-02 18:23:46 +0900

    remove ide waring

--- algorithm/src/boj/ContinuousSum1912.java
+++ algorithm/src/boj/ContinuousSum1912.java
@@ -10,7 +10,7 @@ public class ContinuousSum1912 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(br.readLine());
+		br.readLine();
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int sum = Integer.parseInt(st.nextToken());
--- algorithm/src/boj/StackNumberList1874.java
+++ algorithm/src/boj/StackNumberList1874.java
@@ -3,8 +3,6 @@ package boj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Stack;
 
 public class StackNumberList1874 {
