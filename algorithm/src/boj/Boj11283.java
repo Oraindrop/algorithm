c4176ee5941c44374a9b1ec60dd649707e379884
Choising <chltmdals115@gmail.com>
2020-04-26 19:19:22 +0900

    boj 11282, boj 11283 : 한글, 한글2

---
+++ algorithm/src/boj/Boj11283.java
@@ -0,0 +1,17 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj11283 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		char c = br.readLine().charAt(0);
+		
+		System.out.println((int)c - 44031);
+	}
+
+}
