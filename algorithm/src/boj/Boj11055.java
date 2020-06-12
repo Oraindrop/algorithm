2e4ef7986ef6e13d840316fa5c3b89b6b5e2fc77
Choising <chltmdals115@gmail.com>
2020-06-12 23:29:02 +0900

    boj 11055 : 가장 큰 증가하는 부분 수열

---
+++ algorithm/src/boj/Boj11055.java
@@ -0,0 +1,46 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj11055 {
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		int[] arr = new int[n];
+		int[] dp = new int[n];
+		
+		String[] inputArr = br.readLine().split(" ");
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(inputArr[i]);
+		}
+		
+		for (int i = 0; i < n; i++) {
+			int max = 0;
+			
+			for (int j = 0; j < i; j++) {
+				if (arr[i] > arr[j]) {
+					if (dp[j] > max) {
+						max = dp[j];
+					}
+				}
+			}
+			
+			dp[i] = max + arr[i];
+		}
+		
+		int max = 0;
+		for (int i = 0; i < n; i++) {
+			if (dp[i] > max) {
+				max = dp[i];
+			}
+		}
+		
+		System.out.println(max);
+	}
+
+}
