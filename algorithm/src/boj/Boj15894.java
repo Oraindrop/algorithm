9ab2c1a632b4943510cce2f1b8e1b4cf977e6b1d
Choising <chltmdals115@gmail.com>
2020-07-30 23:32:08 +0900

    boj 15984 : 수학은 체육과목 입니다

---
+++ algorithm/src/boj/Boj15894.java
@@ -0,0 +1,15 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj15894 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println(4l * Long.parseLong(br.readLine()));
+	}
+
+}
