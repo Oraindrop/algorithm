e2090d0cc2a119b84c33177765d1cd6978c012d6
Choising <chltmdals115@gmail.com>
2020-08-27 21:51:36 +0900

    boj 2420 : 사파리월드

---
+++ algorithm/src/boj/Boj2420.java
@@ -0,0 +1,26 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj2420 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		long n = Long.parseLong(st.nextToken());
+		long m = Long.parseLong(st.nextToken());
+		
+		long answer = n - m;
+		
+		if (answer < 0) {
+			answer *= -1;
+		}
+		
+		System.out.println(answer);
+	}
+
+}
