e85d9dbd8b80ce4bf292ecbb05d87028edf649b4
Choising <chltmdals115@gmail.com>
2020-08-29 14:51:23 +0900

    boj 5339 : 콜센터

---
+++ algorithm/src/boj/Boj5339.java
@@ -0,0 +1,19 @@
+package boj;
+
+public class Boj5339 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("     /~\\");
+		System.out.println("    ( oo|");
+		System.out.println("    _\\=/_");
+		System.out.println("   /  _  \\");
+		System.out.println("  //|/.\\|\\\\");
+		System.out.println(" ||  \\ /  ||");
+		System.out.println("============");
+		System.out.println("|          |");
+		System.out.println("|          |");
+		System.out.println("|          |");
+	}
+
+}
