91fcaf1998ab19259e027ed013b53225b87c5203
Choising <chltmdals115@gmail.com>
2020-09-10 23:35:33 +0900

    boj 10991 : 별 찍기 16

---
+++ algorithm/src/boj/Boj10991.java
@@ -0,0 +1,30 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj10991 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n - 1 - i; j++) {
+				sb.append(" ");
+			}
+			
+			for (int j = 0; j <= i; j++) {
+				sb.append("* ");
+			}
+			sb.append(System.lineSeparator());
+		}
+		
+		System.out.println(sb.toString());
+		
+	}
+
+}
