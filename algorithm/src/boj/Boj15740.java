d69174885cc3afdd3040ddb802e205bfaad15a48
Choising <chltmdals115@gmail.com>
2020-10-10 23:41:56 +0900

    boj 15740 : A+B 9

---
+++ algorithm/src/boj/Boj15740.java
@@ -0,0 +1,20 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+
+public class Boj15740 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		BigInteger a = new BigInteger(st.nextToken());
+		BigInteger b = new BigInteger(st.nextToken());
+		System.out.println(a.add(b));
+	}
+
+}
