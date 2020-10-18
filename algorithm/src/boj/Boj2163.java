39603872f45ddf14fb6e81ddd7d06709493d637b
Choising <chltmdals115@gmail.com>
2020-10-18 21:55:38 +0900

    boj 2163 : 초콜릿 자르기

---
+++ algorithm/src/boj/Boj2163.java
@@ -0,0 +1,17 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj2163 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		System.out.println((Integer.parseInt(st.nextToken()) * Integer.parseInt(st.nextToken())) - 1);
+	}
+
+}
