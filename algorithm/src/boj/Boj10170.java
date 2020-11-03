cf6b9299a17b929ca2ec6b825f8d93b9555911d9
Choising <chltmdals115@gmail.com>
2020-11-03 22:53:12 +0900

    boj 5522, 10170 : 짜치는거

---
+++ algorithm/src/boj/Boj10170.java
@@ -0,0 +1,36 @@
+package boj;
+
+public class Boj10170 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated 	method stub
+		StringBuilder sb = new StringBuilder();
+		sb.append("NFC West       W   L  T")
+		.append(System.lineSeparator())
+		.append("-----------------------")
+		.append(System.lineSeparator())
+		.append("Seattle        13  3  0")
+		.append(System.lineSeparator())
+		.append("San Francisco  12  4  0")
+		.append(System.lineSeparator())
+		.append("Arizona        10  6  0")
+		.append(System.lineSeparator())
+		.append("St. Louis      7   9  0")
+		.append(System.lineSeparator())
+		.append(System.lineSeparator())
+		.append("NFC North      W   L  T")
+		.append(System.lineSeparator())
+		.append("-----------------------")
+		.append(System.lineSeparator())
+		.append("Green Bay      8   7  1")
+		.append(System.lineSeparator())
+		.append("Chicago        8   8  0")
+		.append(System.lineSeparator())
+		.append("Detroit        7   9  0")
+		.append(System.lineSeparator())
+		.append("Minnesota      5  10  1");
+		
+		System.out.println(sb.toString());
+	}
+
+}
