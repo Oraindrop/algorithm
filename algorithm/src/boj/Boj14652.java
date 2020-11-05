a6c0a2f6b2a8e4b65d6b7ac7e325f4f34be3400b
Choising <chltmdals115@gmail.com>
2020-11-05 22:32:08 +0900

    boj 14652 : 나는 행복합니다

---
+++ algorithm/src/boj/Boj14652.java
@@ -0,0 +1,21 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj14652 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		st.nextToken();
+		int m = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		
+		System.out.println(k/m + " " + k%m);
+	}
+
+}
