f306bcfb1314e06bcaa7ce3498de7251ab366e58
Choising <chltmdals115@gmail.com>
2020-12-07 23:11:52 +0900

    boj 10810 : 공 넣기

---
+++ algorithm/src/boj/Boj10810.java
@@ -0,0 +1,38 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj10810 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		int[] basket = new int[n+1];
+		
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int value = Integer.parseInt(st.nextToken());
+			
+			for (int j = a; j <= b; j++) {
+				basket[j] = value;
+			}
+		}
+		
+		StringBuilder sb = new StringBuilder();
+		for (int i = 1; i <= n; i++) {
+			sb.append(basket[i]).append(" ");
+		}
+		System.out.println(sb.toString());
+	}
+
+}
