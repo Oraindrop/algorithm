ea135b2884911c74e688536d5c764d5f8a410077
ChoiSing <chltmdals115@gmail.com>
2021-02-06 23:58:24 +0900

    boj 2522 - 별찍기 12 컴파일 에러

---
+++ algorithm/src/boj/Boj2522.java
@@ -0,0 +1,42 @@
+package boj;
+
+ import java.io.BufferedReader;
+ import java.io.IOException;
+ import java.io.InputStreamReader;
+ import java.util.StringTokenizer;
+
+ public class Boj2522 {
+
+ 	public static void main(String[] args) throws NumberFormatException, IOException {
+ 		// TODO Auto-generated method stub
+ 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 		int n = Integer.parseInt(br.readLine());
+ 		StringBuilder sb = new StringBuilder();
+
+ 		for (int i = 1; i <= n; i++) {
+ 		    for (int j = 0; j < n-i; j++) {
+                        sb.append(" ");
+                    }
+
+                    for (int j = 0; j < i; j++) {
+                        sb.append("*");
+                    }
+                    sb.append(System.lineseparator());
+ 		}
+
+                for (int i = 1; i <= n-1; i++) {
+                    for (int j = 0; j < i; j++) {
+                        sb.append(" ");
+                    }
+      
+                    for (int j = 0; j < n-i; j++) {
+                        sb.append("*");
+                    }
+                    sb.append(System.lineseparator());
+                }
+
+ 		System.out.print(sb.toString());
+ 	}
+}
+
+ 
