5490010d3e27149069334f299e2ba3e65c56f74e
Choising <chltmdals115@gmail.com>
2021-02-26 23:25:01 +0900

    boj 17295 : 앤드게임 스포일러

---
+++ algorithm/src/boj/Boj17295.java
@@ -0,0 +1,16 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj17295 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		br.readLine();
+		System.out.println("Avengers: Endgame");
+	}
+
+}
