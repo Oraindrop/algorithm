fe1ccacf80ca76be5764c6eb96fdf35b37af5f3d
Choising <chltmdals115@gmail.com>
2021-03-08 23:40:15 +0900

    boj 18108 : 1998년생인 내가 태국에서는 2541년생

---
+++ algorithm/src/boj/Boj18108.java
@@ -0,0 +1,16 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj18108 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		System.out.println(n - 543);
+	}
+
+}
