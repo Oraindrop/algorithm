09434e3d1fafb256bfb287a868379f8b8e2a6ef1
Choising <chltmdals115@gmail.com>
2021-06-04 00:09:13 +0900

    boj 11059 : 크리 문자열

--- algorithm/src/boj/Boj11059.java
+++ algorithm/src/boj/Boj11059.java
@@ -1,27 +1,45 @@
 package boj;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Boj11059 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		long[] arr = new long[100];
-		long[] arr2 = new long[100];
-		
-		arr[0] = 0;
-		arr[1] = 1;
-		arr[2] = 1;
-		
-		for (int i = 3; i < 100; i++) {
-			arr[i] = arr[i-1] + arr[i-2];
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		int maxSize = input.length() % 2 == 0 ? input.length() : input.length() - 1;
+		int size = maxSize;
+		int answer = 0;
+		while (answer == 0) {
+			int i = 0;
+			while (i + size <= input.length()) {
+				if (isRight(input.substring(i, size+i))) {
+					answer = size;
+					break;
+				}
+				i++;
+			}
+			size-=2;
 		}
-		
-		for (int i = 1; i < 100; i++) {
-			arr2[i] = (arr2[i-1] % 1000000007 + ((arr[i] % 1000000007) * (arr[i] % 1000000007)) % 1000000007) % 1000000007;
+		System.out.println(answer);
+	}
+	
+	static boolean isRight(String s) {
+		String pre = s.substring(0, s.length() / 2);
+		String post = s.substring(s.length() / 2);
+		return sum(pre) == sum(post);
+	}
+	
+	static int sum(String s) {
+		char[] arr = s.toCharArray();
+		int sum = 0;
+		for (char c : arr) {
+			sum += c - '0';
 		}
-		
-		for (int i = 0; i < 100; i++) {
-			System.out.println(arr2[i]);
-		}
+		return sum;
 	}
 
 }
