1a636c4ba10b31eafe0179638117f63c32f91f86
Choising <chltmdals115@gmail.com>
2021-06-13 23:00:36 +0900

    boj 14935 : FA

---
+++ algorithm/src/boj/Boj14935.java
@@ -0,0 +1,16 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Boj14935 {
+
+	// TODO Auto-generated method stub
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		br.readLine();
+		System.out.println("FA");
+	}
+
+}
