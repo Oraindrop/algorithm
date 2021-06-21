3bd22cc682cca222295093016adc84c919019799
Choising <chltmdals115@gmail.com>
2021-06-21 23:31:16 +0900

    boj 15549 : if

---
+++ algorithm/src/boj/Boj15549.java
@@ -0,0 +1,10 @@
+package boj;
+
+public class Boj15549 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("int " + Integer.MIN_VALUE);
+	}
+
+}
