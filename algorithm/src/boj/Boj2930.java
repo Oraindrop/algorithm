0f5169fc3d259c489bca88808899579836a5b8e9
Choising <chltmdals115@gmail.com>
2021-08-10 23:57:53 +0900

    boj 2930 : 가위 바위 보 (구현중)

---
+++ algorithm/src/boj/Boj2930.java
@@ -0,0 +1,74 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
+public class Boj2930 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int r = Integer.parseInt(br.readLine());
+		String base = br.readLine();
+		
+		int n = Integer.parseInt(br.readLine());
+		String[] arr = new String[n];
+		
+		for (int i = 0; i < n; i++) {
+			arr[i] = br.readLine();
+		}
+		
+		int sum = 0;
+		int max = 0;
+		for (int i = 0; i < r; i++) {
+			String baseElement = base.substring(i, i+1); 
+
+			int maxSum = 0; 
+			
+			for (int j = 0; j < n; j++) {
+				String target = arr[j].substring(i, i+1);
+				sum += judge(baseElement, target);
+				maxSum += Math.max(Math.max(judge("R", target), judge("S", target)), judge("P", target));
+			}
+			
+			max += maxSum;
+		}
+		
+		System.out.println(sum);
+		System.out.println(max);
+	}
+	
+	private static int judge(String base, String target) {
+		if (base.equals(target)) 
+			return 1;
+		
+		if (base.equals("R")) {
+			if (target.equals("S")) {
+				return 2;
+			} else {
+				return 0;
+			}
+		}
+		
+		if (base.equals("S")) {
+			if (target.equals("P")) {
+				return 2;
+			} else {
+				return 0;
+			}
+		}
+		
+		if (base.equals("P")) {
+			if (target.equals("R")) {
+				return 2;
+			} else {
+				return 0;
+			}
+		}
+		
+		throw new IllegalArgumentException();
+	}
+
+}
