733401fd788094f43df7e78d7757c0816f4efafa
Choising <chltmdals115@gmail.com>
2021-08-23 23:39:04 +0900

    boj 10864 : 친구

---
+++ algorithm/src/boj/Boj10864.java
@@ -0,0 +1,32 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj10864 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[n];
+		
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			arr[a-1]++;
+			arr[b-1]++;
+		}
+		
+		for (int i = 0; i < n; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+}
