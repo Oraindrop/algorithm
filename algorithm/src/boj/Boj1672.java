9d50debf54ab6f61b92325285b0e0e3f76b16e99
Choising <chltmdals115@gmail.com>
2021-11-28 23:43:13 +0900

    boj 1672 : DNA 해독

--- algorithm/src/boj/Boj1672.java
+++ algorithm/src/boj/Boj1672.java
@@ -3,45 +3,43 @@ package boj;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Boj1672 {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
-		Map<String, String> map = new HashMap<>();
-		map.put("AA" ,"A");
-		map.put("AG" ,"C");
-		map.put("AC" ,"A");
-		map.put("AT" ,"G");
-		map.put("GA" ,"C");
-		map.put("GG" ,"G");
-		map.put("GC" ,"T");
-		map.put("GT" ,"A");
-		map.put("CA" ,"A");
-		map.put("CG" ,"T");
-		map.put("CC" ,"C");
-		map.put("CT" ,"G");
-		map.put("TA" ,"G");
-		map.put("TG" ,"A");
-		map.put("TC" ,"G");
-		map.put("TT" ,"T");
+		char[][] arr = new char[4][4];
+		
+		arr[0] = new char[]{'A', 'C', 'A', 'G'};
+		arr[1] = new char[]{'C', 'G', 'T', 'A'};
+		arr[2] = new char[]{'A', 'T', 'C', 'G'};
+		arr[3] = new char[]{'G', 'A', 'G', 'T'};
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		String input = br.readLine();
-		String[] arr = input.split("");
-		
-		while(n > 2) {
-			arr[n-2] = map.get(arr[n-2] + arr[n-1]);
-			n--;
-		}
-		
+
 		if (n == 1) {
-			System.out.println(arr[0]);
+			System.out.println(input);
 		} else {
-			System.out.println(map.get(arr[0] + arr[1]));
+			char[] inputs = input.toCharArray();
+			char pre = arr[getNumber(inputs[n-1])][getNumber(inputs[n-2])];
+			for (int i = n-3; i >= 0; i--) {
+				pre = arr[getNumber(pre)][getNumber(inputs[i])];
+			}
+			System.out.println(pre);
+		}
+	}
+	
+	static int getNumber(char c) {
+		if (c == 'A') {
+			return 0;
+		} else if (c == 'G') {
+			return 1;
+		} else if (c == 'C') {
+			return 2;
+		} else {
+			return 3;
 		}
 	}
 
